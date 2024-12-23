1a6f1866aa44d86b77ef1f8d7ece82da0da0bd34
Alisson Carvalho <alissonanderson2013@gmail.com>
2024-12-23 15:45:51 -0300

    Versão final

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const backToTopButton = document.getElementById("back-to-top");
-  const aboutSection = document.getElementById("about");
+    const openModalButton = document.getElementById("open-modal");
+    const closeModalButton = document.getElementById("close-modal");
+    const modal = document.getElementById("modal");
+    const fade = document.getElementById("fade")
+    const backToTopButton = document.getElementById("back-to-top");
+    const aboutSection = document.getElementById("about");
 
-  const toggleButtonVisibility = () => {
-    const aboutTop = aboutSection.getBoundingClientRect().top;
-    const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-
-    if (aboutTop <= viewportHeight / 2) {
-      backToTopButton.classList.add("visible");
-    } else {
-      backToTopButton.classList.remove("visible");
+    const toggleModal = () =>{
+      [modal, fade].forEach((el) => {
+        el.classList.toggle("hide");
+      })
     }
-  };
 
-  backToTopButton.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
+    [openModalButton, closeModalButton, fade].forEach((el) => {
+      el.addEventListener("click", () => {
+        toggleModal();
+      })
+    })
+
+    const toggleButtonVisibility = () => {
+      const aboutTop = aboutSection.getBoundingClientRect().top;
+      const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+      if (aboutTop <= viewportHeight / 2) {
+        backToTopButton.classList.add("visible");
+      } else {
+        backToTopButton.classList.remove("visible");
+      }
+    };
+
+    backToTopButton.addEventListener("click", ()=>{
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
     });
-  });
 
-  document.addEventListener("scroll", () => {
-    if (window.scrollY > 0) {
-      document.body.classList.add("scrolled");
-    } else {
-      document.body.classList.remove("scrolled");
-    }
-  });
+    document.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        document.body.classList.add("scrolled")
+      } else {
+        document.body.classList.remove("scrolled")
+      }
+    })
 
-  window.addEventListener("scroll", toggleButtonVisibility);
-});
+    window.addEventListener("scroll", toggleButtonVisibility);
+  });
